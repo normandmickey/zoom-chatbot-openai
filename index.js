f53fa87fd4165e4b89b1292ecb5217dcd68448b2
normandmickey <normandmickey@gmail.com>
2024-10-18 23:16:02 -0400

    Changed Port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { handleZoomWebhook } from './src/zoomWebhookHandler.js';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 
--- src/openai-api.js
+++ src/openai-api.js
@@ -3,6 +3,7 @@ import { getChatbotToken } from './zoomAuth.js';
 import { sendChatToZoom } from './sendChatbotMessage.js';
 import OpenAI from "openai"
 import * as dotenv from 'dotenv';
+import { AskNewsSDK } from '@emergentmethods/asknews-typescript-sdk'
 
 dotenv.config();
 
@@ -12,6 +13,11 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+const ask = new AskNewsSDK({
+  clientId: process.env.ASKNEWS_CLIENT_ID,
+  clientSecret: process.env.ASKNEWS_CLIENT_SECRET,
+  scopes: ['news'],
+})
 
 // Function to handle communication with the OpenAI API
 async function callOpenAIAPI(payload) {
@@ -21,13 +27,28 @@ async function callOpenAIAPI(payload) {
     const history = conversationHistory[userJid] || '';
     const newUserPrompt = `\n\nHuman: ${payload.cmd}\n\nAssistant:`;
     const prompt = history + newUserPrompt;
+    const newsContext = await ask.news.searchNews(
+      { query: "top news" + question, 
+        nArticles: 5, 
+        returnType: 'dicts', 
+        method: 'kw', 
+        categories: ['Politics'] 
+      })
+
+    const context = JSON.stringify(newsContext)
+    
+    //console.log("AskNews: " + context);
 
     const chatCompletion = await openai.chat.completions.create({
-        messages: [{ role: "user", content: question }],
-        model: "gpt-4o-mini",
+        messages: [{ 
+          system: "You are a smart, funny and accurate news reporter.  Summarize the following articles",
+          role: "user", 
+          content: context + ": " + question 
+        }],
+        model: "gpt-4o",
     });
     const completion = chatCompletion.choices[0].message.content;
-    console.log("completion: " + completion)
+    //console.log("completion: " + completion)
 
     // Save conversation history
     conversationHistory[userJid] = prompt + completion;
