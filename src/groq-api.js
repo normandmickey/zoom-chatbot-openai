7582d0f2aa3088791b7da13821fa13632ae8f9a0
normandmickey <normandmickey@gmail.com>
2025-10-13 06:21:26 -0400

    added wolfram alpha

--- src/groq-api.js
+++ src/groq-api.js
@@ -30,7 +30,7 @@ const llm = new ChatGroq({
   model: 'groq/compound',
   compound_custom: {
     "tools": {
-      "enabled_tools": ["web_search","code_interpreter","visit_website","browser_automation"],
+      "enabled_tools": ["web_search","code_interpreter","visit_website","browser_automation","wolfram_alpha"],
       "wolfram_settings": { authorization: process.env.WOLFRAM_ALPHA_API_KEY }
     }
   }
